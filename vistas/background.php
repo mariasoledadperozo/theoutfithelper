d7a9f01d14350a2e974494f894cb7aa358ade283
María Perozo <146212302+mariasoledadperozo@users.noreply.github.com>
2025-08-19 13:03:05 +0200

    19-08-2025: Primer cambio

--- vistas/background.php
+++ vistas/background.php
@@ -23,5 +23,29 @@
     Essence. Signature. Iconic. Statement. Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. 
     Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement.
     Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. 
+    Pop. Tone. Shade. Mood. Look. Essence.  Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. 
+    Glow. Shine. Pop. Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement. Chic. Unique. 
+    Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. 
+    Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement. Chic. Unique. Core. Fierce. Sleek. 
+    Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. Tone. Shade. Mood. Look. 
+    Essence. Signature. Iconic. Statement. Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. 
+    Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement.
+    Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. 
+    Pop. Tone. Shade. Mood. Look. Essence. Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. 
+    Glow. Shine. Pop. Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement. Chic. Unique. 
+    Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. 
+    Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement. Chic. Unique. Core. Fierce. Sleek. 
+    Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. Tone. Shade. Mood. Look. 
+    Essence. Signature. Iconic. Statement. Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. 
+    Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement.
+    Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. 
+    Pop. Tone. Shade. Mood. Look. Essence. Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. 
+    Glow. Shine. Pop. Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement. Chic. Unique. 
+    Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. 
+    Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement. Chic. Unique. Core. Fierce. Sleek. 
+    Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. Tone. Shade. Mood. Look. 
+    Essence. Signature. Iconic. Statement. Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. 
+    Vibe. Aesthetic. Rare. Slay. Glow. Shine. Pop. Tone. Shade. Mood. Look. Essence. Signature. Iconic. Statement.
+    Chic. Unique. Core. Fierce. Sleek. Glam. Trendy. Aunthetic True. Vibe. Aesthetic. Rare. Slay. Glow. Shine. 
     Pop. Tone. Shade. Mood. Look. Essence
 </div>
